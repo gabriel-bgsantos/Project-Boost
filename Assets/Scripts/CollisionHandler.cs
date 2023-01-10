b7a25884db9d8b6905cd3f83bf8e79c62d71c4c6
gpw pc <gabrielbgsantos@hotmail.com>
2023-01-10 08:59:16 -0300

    Tagged things, renamed main Scene to 'Sandbox' and wrote the 'ReloadScene' method

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
-   private void OnCollisionEnter(Collision other) 
-   {
-    switch (other.gameObject.tag){
-        case "Friendly":
-            Debug.Log("Friendly hit");
-            break;
-        case "Fuel":
-            Debug.Log("Fuel hit");
-            break;
-        case "Finish":
-            Debug.Log("Finish hit");
-            break;
-        default:
-            Debug.Log("Did not hit anything");
-            break;
-    }
+    
+    private void OnCollisionEnter(Collision other) 
+    {
+        switch (other.gameObject.tag){
+            case "Friendly":
+                Debug.Log("Friendly hit");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel hit");
+                break;
+            case "Finish":
+                Debug.Log("Finish hit");
+                break;
+            default:
+                ReloadScene();
+                break;
+        }
    }
+
+    private void ReloadScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+        // SceneManager.GetActiveScene().buildIndex; get scene index number
+    }
 }
