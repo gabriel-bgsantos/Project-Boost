8f702fc5e9d13fb8ad3d16410ceeb0f20381796b
gpw pc <gabrielbgsantos@hotmail.com>
2023-01-12 06:07:38 -0300

    Implemented rocket's side booster particles and their scripts (also implemented the new rocket prefab in "Under" scene/level)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    //to do: line 58 and 67 SFX
+    //to do: solve bug of not turning boost particles off after crashing (CollisionHandler)
+
     [SerializeField] float rocketThrust = 1000f;
     [SerializeField] float rocketRotation = 100f;
     [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem leftEngineParticles;
+    [SerializeField] ParticleSystem rightEngineParticles;
 
     Rigidbody rb;
     AudioSource boostAudio;
@@ -29,12 +35,16 @@ public class Movement : MonoBehaviour
     {
         if(Input.GetKey(KeyCode.Space )){
             rb.AddRelativeForce(Vector3.up * Time.deltaTime * rocketThrust);
+            if(!mainEngineParticles.isPlaying){
+                mainEngineParticles.Play();
+            }
             if(!boostAudio.isPlaying){
                 boostAudio.PlayOneShot(mainEngine);
             }
         }
         else{
             boostAudio.Stop();
+            mainEngineParticles.Stop();
         }
     }
 
@@ -43,10 +53,24 @@ public class Movement : MonoBehaviour
         if(Input.GetKey(KeyCode.A)){
             // transform.Rotate(Vector3.forward * Time.deltaTime * rotationRight); // (0, 0, 1)
             ApplyRotation(rocketRotation);
+            if(!leftEngineParticles.isPlaying){
+                leftEngineParticles.Play();
+
+            }
         }
+        
         else if(Input.GetKey(KeyCode.D)){
             // transform.Rotate(Vector3.back * Time.deltaTime * rotationLeft); // (0, 0, -1)
             ApplyRotation(-rocketRotation);
+            if(!rightEngineParticles.isPlaying){
+                rightEngineParticles.Play();
+
+            }
+        }
+        
+        else{
+            leftEngineParticles.Stop();
+            rightEngineParticles.Stop();
         }
     }
 
