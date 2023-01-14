b286882328e2243d6b486b253f1bae1513a303f4
gpw pc <gabrielbgsantos@hotmail.com>
2023-01-14 12:53:08 -0300

    Oscillator script for the Moving Obstacle, moving obstacle material, some new lights / emission changes

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;
+    // [SerializeField] [Range(0,10)] float movementFactor; //create a vector3 for more complex movements | dont need it once we got line 27;
+    
+    Vector3 startingPosition;
+    float movementFactor;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //watch class 58 again for a better undertanding
+        float cycles = Time.time / period; // continually growing over time
+        const float tau = Mathf.PI * 2; // constant value of 6;283 (radian)
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2; // recalculated to go from 0 to 1, so it's cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
